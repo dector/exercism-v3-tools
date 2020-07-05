02898b825582f458315bf6814f5942afdd9cb652
Denys M <dector9@gmail.com>
2020-07-05 08:51:25 +0400

    Update Kotlin to `1.3.72`

--- buildSrc/src/dependencies.kt
+++ buildSrc/src/dependencies.kt
@@ -10,7 +10,7 @@ object Deps {
 }
 
 object Versions {
-    const val kotlin = "1.3.70"
+    const val kotlin = "1.3.72"
     const val kotlin_coroutines = "1.3.2"
     const val kotlin_test = "3.4.2"
 
